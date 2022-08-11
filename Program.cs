2ac4afa858555271b43e8063def7b0d9c7b04f2a
Joseph K Justus <justuskasyoki57@gmail.com>
2022-08-11 15:11:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp11
+{
+    class Program : ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //get all text files in the folder
+                string[] filePaths = Directory.GetFiles(@"D:\current\", "*.txt");
+                List<string> value = filePaths.ToList();
+                //update one by one
+                for (int i1 = 0; i1 < value.Count; i1++)
+                {
+                    string doc = value[i1];
+                    string file  = Path.GetFileName(doc);
+               
+                    string[] lines = System.IO.File.ReadAllLines(doc);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string line = lines[i];
+                        // Use a tab to indent each line of the file.
+                        if (line.Contains("https:"))
+                        {
+                            Link = line;
+
+                        }
+                        else if (line.Contains("TSIN:"))
+                        {
+                            TSIN = line.Substring(line.IndexOf(':') + 1).TrimEnd();
+
+                        }
+                        else if (line.Contains("DATE:"))
+                        {
+                            Date = line.Substring(line.IndexOf(':') + 1).TrimEnd();
+
+                        }
+                        else if (line.Contains("CUSN:"))
+                        {
+                            CUSN = line.Substring(line.IndexOf(':') + 1).TrimEnd();
+
+                        }
+                        else if (line.Contains("CUIN:"))
+                        {
+                            CUIN = line.Substring(line.IndexOf(':') + 1).TrimEnd();
+                        }
+
+
+                    }
+                    File.Copy(Path.Combine(doc), Path.Combine(@"D:\current\New folder", file));
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ ProgramBase.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp11
+{
+    internal class ProgramBase
+    {
+        public static string Date { get; set; }
+        public static string CUSN { get; set; }
+        public static string CUIN { get;  set; }
+        public static string TSIN { get;  set; }
+        public static string Link { get; set; }
+        public string dqbase64 { get;  set; }
+    }
+}
